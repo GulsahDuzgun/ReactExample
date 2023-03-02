7a749cb41310ac2f15611a3e71eb2eab2ef14ee5
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-03 00:14:49 +0300

    React JSX Lists

--- Hello-React/src/app.js
+++ Hello-React/src/app.js
@@ -1,11 +1,26 @@
 //Babelın çevirim yapması için tek bir root element olmalı
-var product = {
+var products = [
+{
         name: "Iphone 15",
         price: "400",
         description: "Iyi telefon",
         colors: ["silver","white","black","red"]
         
-}
+},
+{
+        name: "Iphone 16",
+        price: "80000",
+        description: "Iyi telefon",
+        colors: ["silver","white","black","red"]
+        
+},
+{
+        name: "Iphone 17",
+        price: "4500",
+        description: "Iyi telefon",
+        colors: ["silver","white","black","red"]
+        
+}]
 function formatPrice(product){
         return product.price + "TL";
 }
@@ -19,7 +34,8 @@ function printDescription(product){
 var template =
  <div>
         <h1 id="header1">My First React Application!</h1>
-        <div>
+        { products.map((product,index) => ( 
+                <div className = "product_details" key={index}>
                 <ul>
                         <li>{ product.name }</li>
                         <li>{ product.price }</li>
@@ -30,9 +46,15 @@ var template =
                         { product.description && <li>{ product.description}</li> }     
                         { product.colors.length > 3 ? <li>Birden fazla renk seçeneğimiz vardır</li> : ""} 
                         { ( product.name.length > 3 && product.price > 400 ) ? <li>{ product.name }</li> : <li>Hatalı Ürün</li>}
+                        {product.colors.map((color,index) => (
+                                <li key = {index} >{ color }</li>
+                        ))}
                                     
                 </ul>
         </div>
+                        
+        ))}
+        
 
   </div>//her zaman kapsayıcı bir root eleman olmalıdır
 var rootReact = ReactDOM.createRoot(document.querySelector("#content"));
