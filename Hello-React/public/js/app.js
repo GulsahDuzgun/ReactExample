82b5db0b4ddc45e3a07091c82549721349d61df3
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-03 12:42:21 +0300

    React Data-Binding

--- Hello-React/public/js/app.js
+++ Hello-React/public/js/app.js
@@ -1,6 +1,8 @@
 "use strict";
 
 //Babelın çevirim yapması için tek bir root element olmalı
+var rootReact = ReactDOM.createRoot(document.querySelector("#content"));
+var secilenUrun = [];
 var products = [{
   name: "Iphone 15",
   price: "400"
@@ -13,19 +15,26 @@ var products = [{
 }];
 function addProduct(event, name) {
   console.log(name, event.target);
+  if (!secilenUrun.includes(name)) {
+    secilenUrun.push(name);
+    rootReact.render(renderApp());
+  }
 }
-var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
-  id: "header1"
-}, "My First React Application!"), products.map(function (product, index) {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "product_details",
-    key: index
-  }, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("h2", null, " ", product.name, " ")), /*#__PURE__*/React.createElement("li", null, product.price), /*#__PURE__*/React.createElement("button", {
-    type: "button",
-    onClick: function onClick(event) {
-      addProduct(event, product.name);
-    }
-  }, "Ekle")));
-})); //her zaman kapsayıcı bir root eleman olmalıdır
-var rootReact = ReactDOM.createRoot(document.querySelector("#content"));
-rootReact.render(template);
+function renderApp() {
+  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
+    id: "header1"
+  }, "My First React Application!"), /*#__PURE__*/React.createElement("h2", null, " ", secilenUrun.length, " \xFCr\xFCn se\xE7ilmi\u015Ftir "), products.map(function (product, index) {
+    return /*#__PURE__*/React.createElement("div", {
+      className: "product_details",
+      key: index
+    }, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("h2", null, " ", product.name, " ")), /*#__PURE__*/React.createElement("li", null, product.price), /*#__PURE__*/React.createElement("button", {
+      type: "button",
+      onClick: function onClick(event) {
+        addProduct(event, product.name);
+      }
+    }, "Ekle")));
+  })); //her zaman kapsayıcı bir root eleman olmalıdır
+
+  return template;
+}
+rootReact.render(renderApp());
--- Hello-React/src/app.js
+++ Hello-React/src/app.js
@@ -1,4 +1,7 @@
 //Babelın çevirim yapması için tek bir root element olmalı
+var rootReact = ReactDOM.createRoot(document.querySelector("#content"));
+var secilenUrun = [];
+
 var products = [
 {
         name: "Iphone 15",
@@ -13,24 +16,41 @@ var products = [
         price: "4500",
 }]
 
+
 function addProduct(event, name){
         console.log(name, event.target);
+
+        if(!secilenUrun.includes(name)){
+                secilenUrun.push(name);
+                rootReact.render(renderApp())
+
+        }
 }
 
-var template =
- <div>
-        <h1 id="header1">My First React Application!</h1>
-        { products.map((product,index) => ( 
-                <div className = "product_details" key={index}>
-                <ul>
-                        <li>{ <h2> { product.name } </h2> }</li>
-                        <li>{ product.price }</li> 
-                        <button type="button" onClick = {((event)=> {addProduct(event, product.name)})}  >Ekle</button>                    
-                </ul>
-        </div>               
-        ))}
-        
+function renderApp(){
+        var template =
+        <div>
+               <h1 id="header1">My First React Application!</h1>
+               <h2> { secilenUrun.length} ürün seçilmiştir </h2>
 
-  </div>//her zaman kapsayıcı bir root eleman olmalıdır
-var rootReact = ReactDOM.createRoot(document.querySelector("#content"));
-rootReact.render(template)       
+               { products.map((product,index) => ( 
+                       <div className = "product_details" key={index}>
+                       <ul>
+                               <li>{ <h2> { product.name } </h2> }</li>
+                               <li>{ product.price }</li> 
+                               <button type="button" onClick = {((event)=> {addProduct(event, product.name)})}  >Ekle</button>                    
+                       </ul>
+               </div>               
+               ))}
+               
+       
+        </div>//her zaman kapsayıcı bir root eleman olmalıdır
+        
+        return template;
+}
+
+rootReact.render(renderApp())   
+
+
+
+    
