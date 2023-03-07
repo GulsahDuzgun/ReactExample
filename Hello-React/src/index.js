0c12159a8b12b08d6decb8aed06d50a912ebda21
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-07 18:10:41 +0300

    React Modules

--- Hello-React/src/index.js
+++ Hello-React/src/index.js
@@ -1,4 +1,7 @@
-console.log("deneme")
-console.log("deneme")
-console.log("deneme")
-console.log("deneme")
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+const template = React.createElement("p",{},"Hello React")
+//const template = <p>Hello React</p>
+root.render(template);
