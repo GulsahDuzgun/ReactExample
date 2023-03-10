b2bad8308ec8bbee9a01e671f110b05dbe1cb71e
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-10 14:44:11 +0300

    Http Requests
    fetch API ile request atılarak gelen response önce jsona çevrilerek setState ile ilgili değişkene atanmıştır

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,14 +1,30 @@
+import React from 'react';
 import Navbar from './components/Navbar';
-import User, { UserList } from'./components/UserList';
-function App() {
-  return (
-    <div className="">
-      <Navbar title="Github Finder" />
-      <div className="container mt-3">
-          <UserList/>
+import UserList from'./components/UserList';
+//http isteğini component oluşmadan hemen önce atmamız için life-cycle hooks yapısına ihtiyacımız var . Bunun için class yapısına dönüyoruz
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      users:[]
+    }
+  }
+  componentDidMount(){
+    fetch("https://api.github.com/users").then(response=>response.json())
+    .then(data => this.setState({users:data})
+      //console.log(this.state.users)
+      )
+  }
+  render(){
+    return (
+      <div className="">
+        <Navbar title="Github Finder" />
+        <div className="container mt-3 ">
+            <UserList users ={this.state.users}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- github-finder/src/components/User.js
+++ github-finder/src/components/User.js
@@ -2,19 +2,17 @@ import React from "react";
 
 class User extends React.Component{
     render(){
-        const {avatar_url,username,followers,github_url,website,name} =this.props.userItem
+        const {avatar_url,login,html_url} =this.props.userItem
         return(
-            <div className="card mb-3">
+            <div className="card mb-3 col-8 offset-2">
                 <div className="row g-0">
                     <div className="col-md-3">
-                        <img className="img-fluid rounded-start avatar" src={avatar_url} alt={username} />
+                        <img className="img-fluid rounded-start avatar" src={avatar_url} alt={login} />
                     </div>
                     <div className="col-md-9">
                         <div className="card-body">
-                            <h3 className="card-title">{name}</h3>
-                            <p>Followers: {followers}</p>
-                            <a target="_blank" href={github_url} className="btn btn-outline-primary me-2">Github Profile</a>
-                            <a target="_blank" href={website} className="btn btn-outline-success">WebSite</a>
+                            <h3 className="card-title">{login}</h3>
+                            <a target="_blank" href={html_url} rel="noreferrer" className="btn btn-outline-success btn-sm me-2">Github Profile</a>
                         </div>
                     </div>
                 </div>
