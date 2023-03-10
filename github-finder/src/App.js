c63d6ace1da3b477dbfbe41833a7677d0fb490a3
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-10 12:08:40 +0300

    Default Props
    SınıfAdı.defaultProps={} şeklinde tanımlama ile props olarak hiçbir veri gönderilmediğinde default olarak bu veriler işlenir. Ama gönderilen değer null bile olsa default değeri ezer!!

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,8 +1,8 @@
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div className="bg-primary">
-      Github-Finder
+    <div className="">
+      <Navbar title="Github" />
     </div>
   );
 }
