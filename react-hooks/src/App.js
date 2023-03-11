cd508a09063c89220a5cc9d9086d6c8c9d6cb244
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-11 11:56:34 +0300

    React Hooks
    Class component React.Component sınıfından kalıtım aldığı için state ve lifecycle methodları kullanılabilir .Function component içerisinde ise state ve lifecycle methodlarının eksikliği ise hooks ile giderilir

---
+++ react-hooks/src/App.js
@@ -0,0 +1,12 @@
+//class componet React.Component kalıtım aldığı için içerisinde state, lifecycle methodları kullanılabilir
+//Fuction component içerisinde state-life-cycle gibi özellikleri hooks ile kullanabiliriz. 
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
