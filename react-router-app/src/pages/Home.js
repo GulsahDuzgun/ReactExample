8f21a8a41a1ea6313181df4cae28ef303503a936
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-13 15:01:01 +0300

    Router Links
    NavLink ile auto active class özelliği, Link kullanımı incelendi.

--- react-router-app/src/pages/Home.js
+++ react-router-app/src/pages/Home.js
@@ -1,6 +1,9 @@
+import { NavLink } from "react-router-dom"
 export const Home = () => {
     return (
       <div className='home'>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/About">Hakkında</NavLink>
         <h1>Hoşgeldiniz</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi recusandae autem facilis et non, aut error debitis nobis ullam quae blanditiis repudiandae quidem, impedit ex voluptatibus incidunt voluptas eius neque!</p>
       </div>
