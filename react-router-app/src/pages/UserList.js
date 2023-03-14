63f93f8e4fe22b5b06a74a01a2d58c88ce8557c9
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-14 16:02:17 +0300

    Loaders-2

---
+++ react-router-app/src/pages/UserList.js
@@ -0,0 +1,24 @@
+import { Link, useLoaderData } from "react-router-dom";
+
+export const userLoading = async () => {
+    const user = await fetch('https://jsonplaceholder.typicode.com/users')
+    return user.json();
+}
+
+export const UserList = () => {
+    const data = useLoaderData();
+    return (
+        <div className="users">
+            {
+            data.map((item) => (
+                <div className="card">
+                    <h3><i className="fa-solid fa-caret-right"></i>{ item.name}</h3>
+                    <Link to={ item.id.toString() } key={ item.id }>
+                        <i className="fa-regular fa-square-caret-right"></i>
+                    </Link>
+                </div>
+            ))
+            }
+        </div>
+    )
+}
