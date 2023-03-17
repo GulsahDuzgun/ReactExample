b7bd47297f4ba0a236b7f06e2a45ed0f5ce1744a
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-17 14:13:50 +0300

    Dispatching Actions
    createStore ile oluşturduğumuz state contenti içerisinde değişiklik yapmak istediğimizde stateti güncellemek için dispatch methodu içinde action Objesi gönderilir ve type prp. göre state değişkenleri değiştirilir. Hiçbir değişiklik yoksa default state gönderilir. Store objesi içerisinde herhangi bir değişikliği kontrol eden subscribe methodu ile store.getState() ile alınır.

--- blog-app/src/index.js
+++ blog-app/src/index.js
@@ -10,10 +10,44 @@ const initalStore = {
     count:8
 }
 
-const store = createStore((state = initalStore) => { 
-    return state });
+const store = createStore((state = initalStore, action) => { 
+    
+    switch(action.type){
+        case "INCREASE":
+            const amount = typeof action.increaseAmount === "number" ? action.increaseAmount : 1
+            return {
+                count: state.count + amount
+            }
+        case "DECREASE":
+            return {
+                count: state.count -1
+            }
+        case "RESET":
+            return {
+                count: 0
+            }
+        default:
+            return state;
+    }
+ });
 
-console.log(store.getState())
+store.subscribe(() => {
+    console.log(store.getState());
+ })
+
+store.dispatch({
+    type: "INCREASE",
+    increaseAmount: 12
+})
+
+store.dispatch({
+    type: "DECREASE"
+})
+
+store.dispatch({
+    type: "RESET"
+})
+
 
 root.render(<AppRouter />);
 reportWebVitals();
