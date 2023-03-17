72843346ae1c75b44bef8c0ff1fe12f3412db1c6
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-17 12:30:43 +0300

    createStore()
    createStore() ile container state bilgisine x.getState() ile erişilir.

--- blog-app/src/index.js
+++ blog-app/src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './routers/AppRouter';
 import './App.css'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppRouter />);
+import { createStore } from 'redux'
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const initalStore = {
+    count:8
+}
+
+const store = createStore((state = initalStore) => { 
+    return state });
+
+console.log(store.getState())
+
+root.render(<AppRouter />);
 reportWebVitals();
