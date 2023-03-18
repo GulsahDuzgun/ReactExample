491701cbdf5c93444e9f0a46f7ee1886b66ec700
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-18 12:11:34 +0300

    Add Blog to Redux State
    store.dispatch() içerisinde gönderilen obje addBlog içerisisinde istenilen objenin gönderilerek yine karşı tarafta bekleyen obje içerisine atanarak  type bilgisi eklenen objenin eski state üzerine yeni obje action.blog bilgisinin eklenmesiyle yeni blog store üzerine eklenir ve subscribe metodu çalışır.

--- blog-app/src/index.js
+++ blog-app/src/index.js
@@ -4,24 +4,9 @@ import reportWebVitals from './reportWebVitals';
 import AppRouter from './routers/AppRouter';
 import './App.css'
 import { combineReducers, createStore } from 'redux'
+import { v4 as uuid } from 'uuid'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const state = {
-    blogs: [
-        {
-            id: 1,
-            title: "Blog Title 1",
-            description: "Blog1 description",
-            dateAdded: 0
-        },
-    ],
-    auth: {
-        userId: 1,
-        userName: "GulsahDuzgun",
-        email: "establish@gmail.com"
-    }
-}
-
 const blogState = []
 const authState = {
     userId: 1,
@@ -40,8 +25,25 @@ const authReducer = (state = authState, action) => {
 const blogReducer = (state = blogState, action) => {
    
     switch(action.type) {
+        case 'Add_Blog':
+            return [
+                ...state ,
+                action.blog
+            ]
         default:
             return state;
+    }
+}
+
+const addBlog = ({title = "", description = "", dateAdded = 0}) => {
+    return {
+        type: "Add_Blog",
+        blog: {
+            id: uuid(),
+            title: title,
+            description: description,
+            dateAdded: dateAdded
+        } 
     }
 }
 
@@ -50,7 +52,14 @@ const store = createStore(combineReducers({
     blogs: blogReducer
 }))
 
-console.log(store.getState())
+
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+
+store.dispatch( addBlog({title:"Blog1", description:"BLOG1 Description", dateAdded:3}) )
+addBlog("Blog2", "BLOG2 Description")
+
 
 root.render(<AppRouter />);
 reportWebVitals();
