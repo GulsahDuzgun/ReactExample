77836f6f18291628b43fb71bec57dc514f48f5a0
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-18 16:57:53 +0300

    Update blogReducer.js

--- blog-app/src/reducers/blogReducer.js
+++ blog-app/src/reducers/blogReducer.js
@@ -4,27 +4,36 @@ const blogState = []
 const blogReducer = (state = blogState, action) => {
    
     switch(action.type) {
+        
         case 'Add_Blog':
-            return [
-                ...state ,
-                action.blog
+             return [
+                ...state, action.blog
             ]
+        break;
+
         case 'Remove_Blog':
             return state.filter( ({id }) => {
                 return id !== action.id    
             })
+        break;
+
         case 'Update_Blog':
+
            return state.map((item) => {
-                if( item.id === action.id){
+                if( item.id === action.id ) {
                     return {
                         ...item, ...action.updateObject
                     }
-                }else{
-                    return item
+                }
+                else {
+                    return item;
                 }
             })
+        break;
+
         default:
             return state;
+        break;
     }
 }
 
