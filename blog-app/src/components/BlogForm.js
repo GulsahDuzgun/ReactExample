8a6f58eda116a9e5fb847d5feea784d3c3b89b1c
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-03-19 11:20:24 +0300

    AddBlog Reducer
    BlogForm içinde submit edilince  props ile çağrılıp gönderilen state parametresi İlgili AddBlogPage içerisinde connect ile propsa bağlanan dispatch metdodu içerisine addBlog actionu ile action objesi döndürülür ve  store üzerine eklenir . daha sonra props.history.push ile yönlendirme yapılır

--- blog-app/src/components/BlogForm.js
+++ blog-app/src/components/BlogForm.js
@@ -1,31 +1,60 @@
 import React, { Component }  from "react";
 
-export default class BlogForm extends Component {
-    state = {
-        title: "",
-        description: ""
+export default class BlogForm extends Component  {
+    
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            title: "",
+            description: "",
+            error: ""
+        }
+        this.onTitleChange = this.onTitleChange.bind(this)
     }
-    onTitleChange(e){
+
+    onTitleChange(e) {
         const title =  e.target.value;
-        this.setState({
-            title
-        })
+        this.setState(() => ({ title }))
     }
-    onDescriptionChange(e){
+
+    onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({description}))
     }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+
+        if( !this.state.title || !this.state.description ) {
+            
+            this.setState({
+                error: "Lütfen alanları doldurunuz"
+            })           
+
+        }else {
+            
+            this.setState({ error: "" })
+            this.props.addBlogInfo({
+                title: this.state.title,
+                description: this.state.description,
+                dateAdded: Date.now
+            })        
+        }
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.onSubmit}>
+                    { this.state.error && <p>{ this.state.error }</p>}
                     <div>
                         <input type="text" placeholder="Title" value={this.state.title} onChange={this.onTitleChange} />
                     </div>
                     <div>
                         <textarea placeholder="Description" value={this.state.description} onChange={this.onDescriptionChange} ></textarea>
                     </div>
-                    <button>Add</button>
+                    <button type="submit">Add</button>
                 </form>
             </div>
         )
