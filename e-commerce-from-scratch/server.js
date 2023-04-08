35d2ffed028b2d00fc4f2e3a3748bac5e750c87b
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-04-08 16:24:58 +0300

    express.js-nodemon JSON API
    nodemon ile ayağa kaldırılan server.js içerisinde / get isteğine ilgili json dosyası dönülmüştür.

---
+++ e-commerce-from-scratch/server.js
@@ -0,0 +1,12 @@
+const  express = require('express')
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+//__filename dosya adına kadar gider __dirname ile bulunduğu klasöre ./ gider;
+app.get("/", (req, res) => { // app API bir / get isteği geldiğinde response olarak döndürülen dosya yazılır.
+    res.sendFile(__dirname + "/data/products.json")
+})
+
+
+app.listen(port)
