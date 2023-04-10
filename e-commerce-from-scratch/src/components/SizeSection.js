fc29775745fd681cb2059949baea2c684cc17e38
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-04-10 20:36:45 +0300

    added SizeSection Component

--- e-commerce-from-scratch/src/components/SizeSection.js
+++ e-commerce-from-scratch/src/components/SizeSection.js
@@ -4,6 +4,15 @@ const SizeSection = () => {
   return (
     <div>
         <h3>Sizes:</h3>
+        <div className="sizeContainer">
+          <div className="xs">XS</div>
+          <div className="s">S</div>
+          <div className="m">M</div>
+          <div className="ml">ML</div>
+          <div className="l">L</div>
+          <div className="xl">XL</div>
+          <div className="xxl">XXL</div>
+        </div>
     </div>
   )
 }
