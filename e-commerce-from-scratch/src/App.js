87e491af69f0e898cbfbd4bc3ba1dd2638a7ebdd
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-04-22 23:05:21 +0300

    Update App.js

--- e-commerce-from-scratch/src/App.js
+++ e-commerce-from-scratch/src/App.js
@@ -10,10 +10,10 @@ function App() {
     <BrowserRouter>
       <div>
         <Routes>
-          <Route path="/" exact element={<ProductsRender/>}>
-              <Route path='products' element={<AllProduct/>} />
-              <Route path="products/favorites" element={<FavoriteProducts/>} />
-              <Route path="products/cart" element={<CartProducts/>} />
+          <Route path="/products" exact element={<ProductsRender/>}>
+              <Route index element={<AllProduct/>} />
+              <Route path="favorites" element={<FavoriteProducts/>} />
+              <Route path="cart" element={<CartProducts/>} />
             </Route>
         </Routes>
       </div>
